13cecd3dd11cc26379f55c96d02d8febcc63fc67
Jos33y <josephlagbalu@gmail.com>
2022-10-06 14:53:16 +0100

    dashboard in progress not yet functional

--- front_end/src/Dashboard/components/DashHeader/index.js
+++ front_end/src/Dashboard/components/DashHeader/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles.css";
+import "../styles/index.css";
 import {Link, NavLink} from "react-router-dom";
 
 const DashHeader = ({isActive}) => {
--- front_end/src/Dashboard/pages/Home/index.js
+++ front_end/src/Dashboard/pages/Home/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import '../styles/index.css'
 import DashHeader from "../../components/DashHeader";
+import {Col, Row} from "react-bootstrap";
+import CryptoDashboardCard from "../../components/CryptoDashboardCard";
+import FiatDashboardCard from "../../components/FiatDashboardCard";
 
 const DashboardHome = () => {
     return (
@@ -15,39 +18,42 @@ const DashboardHome = () => {
                     </div>
 
                     <div className="fiat-balance">
-                        <p>Fiat Funds</p>
+                        <h6>Fiat funds</h6>
+                        <Row>
+                            <Col md={4}>
+                                <FiatDashboardCard />
+                            </Col>
+                        </Row>
+                    </div>
 
-                        <div className="fiat-card">
-                            <div className="header">
-                                <div className="balance">
+                    <div className="crypto-balance">
+                        <h6>Crypto assets</h6>
+                        <Row>
+                            <Col md={4}>
+                                <CryptoDashboardCard />
+                            </Col>
+                            <Col md={4}>
+                                <CryptoDashboardCard />
+                            </Col>
+                            <Col md={4}>
+                                <CryptoDashboardCard />
+                            </Col>
+                        </Row>
 
-                                    <div className="balance-header">
-                                        <div className="image-section">
-
-                                        </div>
-                                        <div className="balance-text">
-                                            <p>Balance</p>
-                                            <h3>0.00 NGN</h3>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="info">
-                                    <i className='bx bxs-info-circle'></i>
-                                </div>
-                            </div>
-
-                            <div className="action-button">
-                                <button className="btn btn-md btn-primary"><i className='bx bx-plus'></i> Deposit</button>
-                                <button className="btn btn-md btn-outline-primary"><i className='bx bx-up-arrow-alt'></i> Withdrawal</button>
-                            </div>
-                        </div>
+                        <Row>
+                            <Col md={4}>
+                                <CryptoDashboardCard />
+                            </Col>
+                            <Col md={4}>
+                                <CryptoDashboardCard />
+                            </Col>
+                            <Col md={4}>
+                                <CryptoDashboardCard />
+                            </Col>
+                        </Row>
                     </div>
                 </div>
 
-
-                <div className="dashboard-home">
-                    <h1 className="text-center"> Dashboard Home</h1>
-                </div>
             </div>
 
         </>
