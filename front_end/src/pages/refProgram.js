7a785ca456e61f1642f38e94d1e80d58e96ea77f
Adimula3 <Kolawoleolayinka16@gmail.com>
2022-10-12 08:38:06 -0500

    partial work

--- front_end/src/pages/refProgram.js
+++ front_end/src/pages/refProgram.js
@@ -112,67 +112,91 @@ const RefProgram = () => {
                     </Table>
                 </Container>
             </div>
-            <div className="advice">
-                <Container>
-                    <Row>
-                        <Col lg={8}>
-                            <div className="d-flex">
-                                <Link to="" className="advice-links">Example1</Link>
-                                <Link to="" className="advice-links">Example2</Link>
+            <div className="advice d-flex">
+                            <div className="advice-content">
+                                <div className="advice-links d-flex">
+                                    <Link to="" className="advice-links">Example1</Link>
+                                    <Link to="" className="advice-links">Example2</Link>
+                                </div>
+                                <p className="advice-details">You have 10 referrals, each of which trades $40,000 per week.
+                                    The total sum of the trades of all of your referalls will be $400,000 per week.
+                                    They will pay $1000 commission, and you will receive 50% of this amount, i.e.
+                                    $500, This means, <span>per month on 10 such referrals you will earn $2000</span></p>
                             </div>
-                            <p>You have 10 referrals, each of which trades $40,000 per week.
-                                The total sum of the trades of all of your referalls will be $400,000 per week.
-                                They will pay $1000 commission, and you will receive 50% of this amount, i.e.
-                                $500, This means, per month on 10 such referrals you will earn $2000</p>
-                        </Col>
-                        <Col lg={4}>
-                            <p>Advice
-                                Your task is to increase the trading volume of your referrals.
-                                Therefore it's logical to spend time on training your referrals, teaching them
-                                the principles of how the exchange works, etc.
-                                You might have a lot of referalls, each trading in small volumes,
-                                or it could be a small network of whales 😉 Choose your strategy.</p>
-                        </Col>
-                    </Row>
-                </Container>
+                            <div className="advice-content2">
+                                <h6>Advice</h6>
+                                <p>
+                                    Your task is to increase the trading volume of your referrals.
+                                    Therefore it's logical to spend time on training your referrals, teaching them
+                                    the principles of how the exchange works, etc.
+                                   </p>
+                                <p> You might have a lot of referalls, each trading in small volumes,
+                                    or it could be a small network of whales 😉 Choose your strategy.</p>
+                            </div>
             </div>
-            <div className="d-flex">
+            <div className="blogger d-flex">
                     <div>
                         <img src={influencer} alt="" className="influencer"/>
                     </div>
                     <div>
-                        <h3>Blogger ?</h3>
-                        <p>If you have a blog, lots of subscribers or you would like to create an advertising campaign,
+                        <h3 className="title">Blogger ?</h3>
+                        <p className="message">If you have a blog, lots of subscribers or you would like to create an advertising campaign,
                             get in touch with us using this address - support@kuna.io</p>
-                        <p>We have special terms for you.
-                            P.S.Mum said that it is not a profession but we know that !</p>
+                        <p>We have special terms for you.</p>
                     </div>
             </div>
-            <div>
+            <div className="QA">
                 <Container>
-                    <Accordion defaultActiveKey="0" flush>
+                    <h1>You asked and we answered</h1>
+                    <Accordion className="accordion" defaultActiveKey="0" flush>
                         <Accordion.Item eventKey="0">
-                            <Accordion.Header>Accordion Item #1</Accordion.Header>
-                            <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                culpa qui officia deserunt mollit anim id est laborum.
+                            <Accordion.Header>How is the commission calculated from my referalls ?</Accordion.Header>
+                            <Accordion.Body className="accordion-body">
+                                The trading volume is calculated for each pair, then all summarized.
+
+                                Referral 1. One of your referrals has bought 1 BTC for a price of $4000, and then after 1 minute, sold it for a price of $4010. The value of the trades will be $8010. The total commission will be $20
+                                Referral 2. Another of your referrals has traded on the pair ETH/USDT. And during the day he has bought and sold 10 ETH five times.
+
+                                This means that, with an ETH price of $135, over the course of the day, the value of trades is $6750. The size of the commission paid is $16.87
+
+                                This means that, for one day the volume of trades of all of your referrals will make up $14750. Throughout the week, with these trades, you will receive a total of $103,250 of trading value
+
+                                The total amount of commission paid for the week is $258
                             </Accordion.Body>
                         </Accordion.Item>
                         <Accordion.Item eventKey="1">
-                            <Accordion.Header>Accordion Item #2</Accordion.Header>
+                            <Accordion.Header>When will the bonus be calculated</Accordion.Header>
+                            <Accordion.Body className="accordion-body">
+                                Referral bonuses are accrued every Tuesday at 00:00 (UTC). The bonuses will appear on the wallets of each asset.
+                                All transactions appear in the replenishment history.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>If my referrals trading volume is less than $400 per week, will I still receive anything ?</Accordion.Header>
+                            <Accordion.Body className="accordion-body">
+                                Probably not. It depends on the size of the commission fee. If your referrals for the week have paid less than $1 in commission, then you will not receive anything.
+                                A good reason to encourage your referrals to trade or to expand your referral network. 😉
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header>Can we be mutual referrals to each other ?</Accordion.Header>
+                            <Accordion.Body className="accordion-body">
+                                Unfortunately not ! A referral can only be an account that was registered after your own ?
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="4">
+                            <Accordion.Header>Can I create a new account, in order to be a referral for myself ?</Accordion.Header>
                             <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                culpa qui officia deserunt mollit anim id est laborum.
+                                Yes, of course ! But there is little point in this.
+                                It's far better to develop your referral network of friends and acquintances, who will be trading larger amounts.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="5">
+                            <Accordion.Header>Where can I see the bonuses from the referral program ?</Accordion.Header>
+                            <Accordion.Body className="accordion-body">
+
+                                On the page 'my bonuses' you will see the approximate asset value, which will be credited on Tuesday.
+                                Additionally, all referral program transactions are available on the assets pages in the deposit history.
                             </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
